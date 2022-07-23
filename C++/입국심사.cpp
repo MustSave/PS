8261d6e6f51b9bb0e48736e224da48334617d97c
MustSave <58774251+MustSave@users.noreply.github.com>
2022-07-24 01:43:15 +0900

    0724 mod

--- C++/입국심사.cpp
+++ C++/입국심사.cpp
@@ -3,38 +3,76 @@
 #include <queue>
 
 using namespace std;
+#pragma region Priority Queue
+// long long solution(int n, vector<int> times) {
+//     priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
+//     pair<long long, int> t;
+//     vector<int>::iterator iter;
 
-long long solution(int n, vector<int> times) {
-    priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-    pair<long long, int> t;
-    vector<int>::iterator iter;
+// #pragma region Timeout해결을 위한 부분
+//     double workSpeed = 0;
+//     for (iter = times.begin(); iter < times.end(); iter++)
+//     {
+//         workSpeed += 1.0f / *iter;
+//     }
 
-    double d=0;
+//     long long eta = (long long)(n / workSpeed);
 
-    for (iter = times.begin(); iter < times.end(); iter++)
+//     for (iter = times.begin(); iter < times.end(); iter++)
+//     {
+//         long long i = eta / *iter;
+//         pq.push(make_pair(*iter * (i+1), *iter));
+//         n -= i;
+//     }
+// #pragma endregion
+
+//     while (n > 0)
+//     {
+//         t = pq.top();
+//         pq.pop();
+//         pq.push(make_pair(t.first + t.second, t.second));
+//         n--;
+//     }
+
+//     return t.first;
+// }
+#pragma endregion
+
+#pragma region Binary Search // 대충은 알겠는데 확실하게는 모르겠다...
+long long solution(int n, vector<int> times)
+{
+    long long left = 0, mid = 0, right = 1e9 * n, answer = right;
+
+    double workSpeed = 0;
+    int lazy = 0;
+    for (int i = 0; i < times.size(); i++)
     {
-        d += 1.0f / *iter;
+        workSpeed += (double)1.0 / times[i];
+        if (times[i] > lazy)
+            lazy = times[i];
     }
 
-    long long eta = (long long)(n / d);
+    left = n / workSpeed - lazy;
+    right = answer = left + lazy + lazy;
 
-    for (iter = times.begin(); iter < times.end(); iter++)
+    while (left <= right)
     {
-        long long i = eta / *iter;
-        pq.push(make_pair(*iter * (i+1), *iter));
-        n -= i;
+        mid = (left + right) >> 1;
+
+        long long tmp = 0;
+        for (auto x : times) // double이 아니라 long으로 하는 이유???
+            tmp += mid / x;
+
+        if (tmp >= n)
+            right = mid - 1;
+        else
+            left = mid + 1;
     }
 
-    while (n > 0)
-    {
-        t = pq.top();
-        pq.pop();
-        pq.push(make_pair(t.first + t.second, t.second));
-        n--;
-    }
-
-    return t.first;
+    return left;
 }
+
+#pragma endregion
 
 #include<iostream>
 void main()
